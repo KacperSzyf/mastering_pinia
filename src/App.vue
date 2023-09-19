75d41d6e154e7bd4765ae07f2d84ec8322d91555
Kacper <kacper.szyf@icloud.com>
2023-09-19 21:44:25 +0100

    removed routes and views

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,9 @@
 <template>
+  <main>
+    <!-- headin -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia pineapple logo" />
+      <h1>pinia tasks</h1>
+    </header>
+  </main>
 </template>
