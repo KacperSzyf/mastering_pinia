2401380514b3dfae51a29395a1771e1640e4c715
Kacper <kacper.szyf@icloud.com>
2023-09-19 22:27:17 +0100

    added favourite tasks

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,15 @@
     </header>
     <!-- task list -->
     <div class="task-list">
+      <p>all tasks</p>
       <div v-for="task in taskStore.tasks" :key="task.id">
-        <TaskDetails :task="task"/>
-        <!-- <p>{{ task.title }}</p> -->
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+    <div class="task-list">
+      <p>favourites tasks</p>
+      <div v-for="task in taskStore.favourites" :key="task.id">
+        <TaskDetails :task="task" />
       </div>
     </div>
   </main>
