0f49aa7ccbd74aad9d4e7f85c7bcbbf4dcf55454
Kacper <kacper.szyf@icloud.com>
2023-09-19 22:18:42 +0100

    added store state to App

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,18 @@
     <!-- task list -->
     <div class="task-list">
       <div v-for="task in taskStore.tasks" :key="task.id">
-        <p>{{ task.title }}</p>
+        <TaskDetails :task="task"/>
+        <!-- <p>{{ task.title }}</p> -->
       </div>
     </div>
   </main>
 </template>
 
 <script>
+import TaskDetails from './components/TaskDetails.vue'
 import { useTaskStore } from './stores/TaskStore'
 export default {
+  components: { TaskDetails },
   setup() {
     const taskStore = useTaskStore()
 
