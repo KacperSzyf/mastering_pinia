b0e798a3c8de5fc95e94c3545e9fabb35d373ff7
Kacper <kacper.szyf@icloud.com>
2023-09-19 22:31:05 +0100

    added filtering to tasks

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,19 @@
       <img src="./assets/pinia-logo.svg" alt="pinia pineapple logo" />
       <h1>{{ taskStore.name }}</h1>
     </header>
+    <!-- filer -->
+    <nav class = "filter">
+      <button @click="filter = 'all'">show all</button>
+      <button @click="filter = 'favourites'"> show favourites</button>
+    </nav>
     <!-- task list -->
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'all'">
       <p>all tasks</p>
       <div v-for="task in taskStore.tasks" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'favourites'">
       <p>favourites tasks</p>
       <div v-for="task in taskStore.favourites" :key="task.id">
         <TaskDetails :task="task" />
@@ -22,14 +27,16 @@
 </template>
 
 <script>
+import { ref } from 'vue'
 import TaskDetails from './components/TaskDetails.vue'
 import { useTaskStore } from './stores/TaskStore'
 export default {
   components: { TaskDetails },
   setup() {
     const taskStore = useTaskStore()
+    const filter = ref('all')
 
-    return { taskStore }
+    return { taskStore, filter }
   }
 }
 </script>
